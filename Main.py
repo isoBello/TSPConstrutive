003fce8a1890fbd91ee1f79c57ff9a9d08707e29
isoBello <isabellamfreitas2@gmail.com>
2021-01-20 11:48:56 -0300

    Add distances method

--- Main.py
+++ Main.py
@@ -1,75 +1,93 @@
 #!/TSPConstrutive/venv/bin python3.6
 # -*- coding: utf-8 -*-
-
+import math
 import sys
 from collections import defaultdict
-import numpy as np
 
 
-def graph():
+def create_graph():
+    vertices = []
     coordenates = {}
-    head = {}
 
     try:
         with open(sys.argv[1]) as file:
-            for inf in range(0, int(file.readline())[5]):
-                infos = inf.split(" ")
-                head[infos[0]] = infos[:0]
-            vertices = [x for x in range(0, head.get("DIMENSION:"))]
+            head = [next(file) for x in range(5)]
+            qtd_vertices, distance_type = infogetter(head)
+            vertices = [x for x in range(1, qtd_vertices+1)]
 
-            for line in file.readline()[5:]:
+            lines = file.readlines()
+            for line in lines[1:]:
                 try:
-                    values = line.split(" ")
-                    v = values[0]
-                    x = values[1]
-                    y = values[2]
-
+                    v, x, y = valuesgetter(line)
                     coordenates[v] = (x, y)
-                except ValueError:
+                except TypeError:
                     continue
+
     except FileNotFoundError:
         print("File not found, please try another filename.")
 
-    if head.get("EDGE_WEIGHT_TYPE") == "EUC_2D":
+    if "EUC_2D" in distance_type:
         calculate_distances(vertices, coordenates, 0)
     else:
         calculate_distances(vertices, coordenates, 1)
+
+
+def valuesgetter(line):
+    values = line.split(" ")
+    try:
+        v = int(values[0])
+        x = float(values[1])
+        y = float(values[2])
+        return v, x, y
+    except ValueError:
+        pass
+
+
+def infogetter(lhead):
+    for inf in lhead:
+        if 'DIMENSION' in inf:
+            qtd_v = int(inf.split(" ")[1])
+        elif 'EDGE_WEIGHT_TYPE' in inf:
+            dist_type = inf.split(" ")[1]
+    return qtd_v, dist_type
 
 
 def calculate_distances(lvs, lcoords, type):
     count = 1
     distances = defaultdict(list)
 
-    while count < len(lvs):
+    while count <= len(lvs):
         coords = lcoords.get(count)
         xi = coords[0]
         yi = coords[1]
         for k, v in lcoords.items():
-            if k != count:
-                xj = v[0]
-                yj = v[1]
-                if type == 0:
-                    distances[count].append(calculate_euclidian_dist(xi, yi, xj, yj))
-                else:
-                    distances[count].append(calculate_pseudo_euclidian_dist(xi, yi, xj, yj))
+            xj = v[0]
+            yj = v[1]
+            if type == 0:
+                distances[count].append(calculate_euclidian_dist(xi, yi, xj, yj))
+            else:
+                distances[count].append(calculate_pseudo_euclidian_dist(xi, yi, xj, yj))
         count += 1
-    print(distances)
 
 
 def calculate_euclidian_dist(x_i, y_i, x_j, y_j):
     xd = x_i - x_j
     yd = y_i - y_j
-    return int(np.sqrt(pow(xd, 2) + pow(yd, 2)))
+    return int(math.sqrt(pow(xd, 2) + pow(yd, 2)))
 
 
 def calculate_pseudo_euclidian_dist(x_i, y_i, x_j, y_j):
     xd = x_i - x_j
     yd = y_i - y_j
 
-    rij = np.sqrt(pow(xd, 2) + pow(yd, 2)/10.0)
+    rij = math.sqrt(pow(xd, 2) + pow(yd, 2)/10.0)
     tij = int(rij)
 
     if tij < rij:
         return tij + 1
     else:
         return tij
+
+
+if __name__ == "__main__":
+    create_graph()
