d0f926b6dc5a3ce4ffe5e0b7332d591207f3c3fe
isoBello <isabellamfreitas2@gmail.com>
2021-01-20 18:50:44 -0300

    Fixing shell script for att file

--- run.sh
+++ run.sh
@@ -8,9 +8,13 @@ set dirin "Entradas/EUC_2D/"
 
 set dirout "Saidas/"
 
+set dirin "Entradas/att48.tsp"
+
+time -v python3 Main.py "$dirin" "$dirout"
+
 #python3 Main.py "$dirin" "$dirout"
-for file in "$dirin"/*
-	time python3 Main.py "$file" "$dirout"
-end
+#for file in "$dirin"/*
+#	time -v python3 Main.py "$file" "$dirout"
+#end
 
 deactivate
